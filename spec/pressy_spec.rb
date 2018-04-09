3a1e7f737d666957567375167b2664aeb7a4bdf7
Matt Moriarity <mmoriarity@apple.com>
2018-04-08 19:18:13 -0500

    Remove failing test

--- spec/pressy_spec.rb
+++ spec/pressy_spec.rb
@@ -2,8 +2,5 @@ RSpec.describe Pressy do
   it "has a version number" do
     expect(Pressy::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
+
